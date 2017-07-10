1d31518dd35dfc786cc7d525ecda83c0e5935903
chonganong <chongan@uchicago.edu>
2017-07-10 11:27:18 -0500

    emptying output dir

--- stata_practice_r.R
+++ stata_practice_r.R
@@ -1,18 +1,20 @@
 rm(list=ls())
-
+install.packages('reader')
+install.packages('dplyr')
 library(reader)
-library(data.table)
-library(tibble)
 library(dplyr)
-
+library(stargazer)
 setwd("~/GitHub/stata_practice/data")
 
 clean <- lapply(list.files(,pattern="*.csv"), read.csv) %>%              # reads a list of .csv files into a list of data-frames
-            bind_rows %>%                                                # appends them into a single data-frame
-            select(Competition_Month, Competition_Year, 
-                   Home_City, Home_Section, Home_State, Points, Rank)    # drops unwanted variables
+  bind_rows %>%                                                # appends them into a single data-frame
+  select(Competition_Month, Competition_Year, 
+         Home_City, Home_Section, Home_State, Points, Rank)    # drops unwanted variables
+
+setwd("~/stata-practice-test-chongan")
 
 clean <- clean %>% mutate(Home_State = replace(Home_State, Home_City=="Guam","GU"))
+clean <- clean %>% mutate(Home_State = replace(Home_State, Home_State=="Fl","FL"))
 clean <- clean %>% mutate(Home_State = replace(Home_State, nchar(Home_State)!=2, "Foreign"))
 clean <- clean %>% mutate(Home_State = replace(Home_State, Home_State== "AB" | Home_State== "BC" |
                                                  Home_State== "CZ" | Home_State== "ON" |
@@ -35,4 +37,8 @@ clean <- clean %>%
   mutate(Home_Section_Count = n())
 
 t.test(Points~Hill_Indicator, clean)
-
+stargazer(summary(clean$Home_State), flip=T, type = "text")
+reg1<-lm(Rank~Points, data=clean)
+reg2<-lm(Rank~Points+Home_State, data=clean)
+reg3<-lm(Rank~Points+Home_State+as.factor(Competition_Year), data=clean)
+stargazer(reg1,reg2,reg3, type="text")
